12472214c20516062e76d6f8b532285f6f27aa8e
Sean <sean.lorman@gmail.com>
2020-04-27 09:46:34 -0600

    Add an emit.io so the chats are broadcasted to everyone in the chat room

--- index.js
+++ index.js
@@ -7,8 +7,27 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+// });
+
+// io.on("connection", (socket) => {
+//   socket.on("chat message", (msg) => {
+//     console.log("message: " + msg);
+//   });
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
 
 http.listen(3000, () => {
   console.log("listening on *:3000");
